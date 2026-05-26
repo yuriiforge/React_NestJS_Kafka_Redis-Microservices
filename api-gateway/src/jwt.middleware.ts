9f8cc15fe45546c7e9bc877cfe96234ae511306e
YuriiStepaniuk <yurii.stepaniuk.dev@gmail.com>
2026-05-26 16:00:02 +0300

    Configure api-gateway, connect prisma db and generate initial schema, enable symlinking for shared modules, create jwt payload

---
+++ api-gateway/src/jwt.middleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import * as jwt from 'jsonwebtoken';
+import { Role } from '@ecommerce/shared/src/enums/role.enum';
+
+interface CustomJwtPayload extends jwt.JwtPayload {
+  userId: string;
+  email: string;
+  role: Role;
+}
+
+export function jwtMiddleware(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) return res.status(401).json({ message: 'Unauthorized' });
+  try {
+    const payload = jwt.verify(
+      token,
+      process.env.JWT_SECRET!,
+    ) as CustomJwtPayload;
+    req.headers['x-user-id'] = payload.userId;
+    req.headers['x-user-role'] = payload.role;
+    req.headers['x-user-email'] = payload.email;
+    next();
+  } catch {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+}
--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,72 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { jwtMiddleware } from './jwt.middleware';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // protected routes — JWT check first
+  app.use('/api/orders', jwtMiddleware);
+  app.use('/api/products', jwtMiddleware);
+  app.use('/api/search', jwtMiddleware);
+  app.use('/api/analytics', jwtMiddleware);
+  app.use('/api/events', jwtMiddleware);
+
+  // proxies
+  app.use(
+    '/api/auth',
+    createProxyMiddleware({
+      target: 'http://auth-service:3006',
+      changeOrigin: true,
+      pathRewrite: { '^/api/auth': '/auth' },
+    }),
+  );
+
+  app.use(
+    '/api/orders',
+    createProxyMiddleware({
+      target: 'http://order-service:3001',
+      changeOrigin: true,
+      pathRewrite: { '^/api/orders': '/orders' },
+    }),
+  );
+
+  app.use(
+    '/api/events',
+    createProxyMiddleware({
+      target: 'http://order-service:3001',
+      changeOrigin: true,
+      pathRewrite: { '^/api/events': '/events' },
+    }),
+  );
+
+  app.use(
+    '/api/products',
+    createProxyMiddleware({
+      target: 'http://product-service:3007',
+      changeOrigin: true,
+      pathRewrite: { '^/api/products': '/products' },
+    }),
+  );
+
+  app.use(
+    '/api/search',
+    createProxyMiddleware({
+      target: 'http://search-service:3005',
+      changeOrigin: true,
+      pathRewrite: { '^/api/search': '/search' },
+    }),
+  );
+
+  app.use(
+    '/api/analytics',
+    createProxyMiddleware({
+      target: 'http://analytics-service:3004',
+      changeOrigin: true,
+      pathRewrite: { '^/api/analytics': '/analytics' },
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
