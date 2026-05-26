fcf16ca90b6677585bf606d0e82981851e054cc2
YuriiStepaniuk <yurii.stepaniuk.dev@gmail.com>
2026-05-26 09:35:13 +0300

    Init repository

---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Hello from the new app</div>;
+};
+
+export default App;
---
+++ frontend/src/pages/Orders.tsx
@@ -0,0 +1,5 @@
+const Orders = () => {
+  return <div>Hello from orders page!</div>;
+};
+
+export default Orders;
