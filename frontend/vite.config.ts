aa12552950896f0be93cdee8a46b6ae0e28c6c1c
YuriiStepaniuk <yurii.stepaniuk.dev@gmail.com>
2026-06-09 16:54:43 +0300

    fix issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,12 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
   },
   resolve: {
     alias: {
